36d13c6cb726e50fa8ecd328edf5af3679cb7c7b
Vo Minh Quan <13520668@gm.uit.edu.vn>
2016-03-11 10:34:07 +0700

    grapheneDB

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module DwN
     end
 
     config.neo4j.session_type = :server_db
+    config.neo4j.session_path = ENV["GRAPHENEDB_URL"] || 'http://localhost:7474'
     config.neo4j.session_options = { basic_auth: { username: 'neo4j', password: '025547584'} }
 
     # Configure where the embedded neo4j database should exist
